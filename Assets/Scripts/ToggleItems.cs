54888ebcf6af270c5a57013172f3312f7d9a4b3e
Yaroslava Boyko <ysboyko@gmail.com>
2020-10-05 17:01:29 -0400

    Adjustemtents
    - added plant script
    - changed first two pages to stand still pages

---
+++ Assets/Scripts/ToggleItems.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleItems : MonoBehaviour
+{
+    public Button plusButton;
+    public GameObject ItemLayouts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plusButton.onClick.AddListener(() => OnPlusButtonClicked());
+        ItemLayouts.gameObject.SetActive(false);
+    }
+
+    void OnPlusButtonClicked()
+    {
+        if (ItemLayouts.gameObject.activeSelf == true)
+        {
+            ItemLayouts.gameObject.SetActive(false);
+        } else
+        {
+            ItemLayouts.gameObject.SetActive(true);
+        }
+    }
+}
