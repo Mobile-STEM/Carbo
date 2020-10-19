b4eed25ae511583c4f46ff3e9398e89347d79a54
Yaroslava Boyko <ysboyko@gmail.com>
2020-10-18 20:11:17 -0400

    Added additional functionality to screen 2 and 3

---
+++ Assets/Scripts/ARTextUpdate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARTextUpdate : MonoBehaviour
+{
+    public Button backButton;
+    public Button nextButton;
+    public Text nextButtonText;
+    public Text text;
+
+    public string[] textArray;
+    public int arrayPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextButton.onClick.AddListener(() => OnNextButtonClicked());
+        backButton.onClick.AddListener(() => OnBackButtonClicked());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnNextButtonClicked()
+    {
+    }
+
+    void OnBackButtonClicked()
+    {
+        if (arrayPosition == 0)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Molecules");
+        }
+        else
+        {
+            arrayPosition--;
+            text.text = textArray[arrayPosition];
+        }
+    }
+}
